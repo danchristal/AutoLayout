3b7f376084b70e7f47196a2c7c460028fa0f5b07
Dan <dan.christal@gmail.com>
2016-09-13 15:12:33 -0700

    Auto Layout using Storyboard

---
+++ AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Dan Christal on 2016-09-13.
+//  Copyright © 2016 Dan Christal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
